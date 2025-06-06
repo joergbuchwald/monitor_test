3112c885a4cb3abc2fcef203db3d0aa86ab46450
Jörg Buchwald <joerg.buchwald@ufz.de>
2025-06-06 15:21:11 +0200

    update monitor

--- ogs-monitor.py
+++ ogs-monitor.py
@@ -35,10 +35,13 @@ from ogstools.logparser.regexes import (
     IterationStart,
     TimeStepConvergenceCriterion,
     ComponentConvergenceCriterion,
-    SimulationExecutionTime
+    SimulationExecutionTime,
+    SimulationStartTime
 )
 
-from bokeh.models import ColumnDataSource, Select, Range1d, HoverTool
+from bokeh.models import ColumnDataSource, Select, Range1d, HoverTool, Button
+
+from bokeh.models.widgets import Slider
 
 if os.path.exists(sys.argv[1]):
     file_path = sys.argv[1]
@@ -46,6 +49,12 @@ else:
     print("File path does not exist. Please provide a valid path.")
     sys.exit(1)
 print(f"Monitoring log file: {file_path}")
+if len(sys.argv) > 2:
+    print("Set update interval to:", sys.argv[2])
+    update_interval = int(sys.argv[2])
+else:
+    print("No update interval provided, using default of 1000 ms.")
+    update_interval = 100
 
 block = True
 data_source = ColumnDataSource(data = {"time_step": [], "step_size": [], "assembly_time": [], "linear_solver_time": [], "step_start_time": [], "iteration_number": []})
@@ -69,6 +78,8 @@ print("Starting observer...")
 
 observer.start()
 
+
+os.utime(original_file, None)  # Update the file's last modified time to ensure it is read from the beginning
 
 logo = "ogs_textlogo.png"
 
@@ -117,14 +128,34 @@ hover = HoverTool(
     ]
 )
 fig_2.add_tools(hover)
+
+#button = Button(label = "Stop / Start Observer", button_type = "danger")
+range_ts = 0
+range_iter = 100
+
+slider_ts = Slider(start = 0, end = 100,
+                value = range_ts, step = 10,
+                title = "Time Step Window Size")
+slider_iterations = Slider(start = 0, end = 100,
+                value = range_iter, step = 10,
+                title = "Iteration Window Size")
+
 dropdown_lin = Select(title="Linear Scale Plots:", value="Iteration Number", options=["Iteration Number", "Assembly Time", "Linear Solver Time"])
 
 dropdown_log = Select(title="Logarithmic Scale Plots", value="dx_x_0", options=["Step Size", "Timestep Start Time", "dx_x", "dx_x_0", "dx_x_1", "dx_x_2", "dx_x_3", "dx_x_4", "dx_x_5"])
 
 def update_figure():
-    global records, observer, data_source, data_source_iter, block
+    global records, observer, data_source, data_source_iter, block, handler, original_file, fig_1, fig_2
     try:
         item = records.get(block=block)
+        #if isinstance(item, SimulationStartTime):
+        #    print(f"Simulation start time: {item.start_time}")
+        #    block = True
+         #   if not observer.is_alive():
+        #        print("Observer is not alive, starting a new one.")
+        #        observer.schedule(handler, path=str(original_file.parent), recursive=False)
+        #        print("Starting observer...")
+        #        observer.start()
         if isinstance(item, Termination):
             print(f"Consumer: Termination signal ({item}) received. Exiting.")
             block = False
@@ -136,9 +167,25 @@ def update_figure():
             observer.stop()
             observer.join()
         elif isinstance(item, TimeStepStart):
-            print(f"Timestep: {item.time_step}, Step size: {item.step_size}")
-            new_row = {"step_size": [item.step_size,], "time_step": [item.time_step,], "assembly_time": [0], "linear_solver_time": [0], "step_start_time": [item.step_start_time], "iteration_number": [0]}
-            data_source.stream(new_row)
+            print(f"Timestep: {item.time_step}, Step size: {item.step_size}")           
+            if range_ts == 0:
+                new_row = {"step_size": [item.step_size,], "time_step": [item.time_step,], "assembly_time": [0], "linear_solver_time": [0], "step_start_time": [item.step_start_time], "iteration_number": [0]}
+                data_source.stream(new_row)
+            else:
+                step_size = data_source.data["step_size"] + [item.step_size]
+                time_step = data_source.data["time_step"] + [item.time_step]
+                assembly_time = data_source.data["assembly_time"] + [0]
+                linear_solver_time = data_source.data["linear_solver_time"] + [0]
+                step_start_time = data_source.data["step_start_time"] + [item.step_start_time]
+                iteration_number = data_source.data["iteration_number"] + [0]
+                step_size = step_size[-range_ts:] if len(step_size) > range_ts else step_size
+                time_step = time_step[-range_ts:] if len(time_step) > range_ts else time_step
+                assembly_time = assembly_time[-range_ts:] if len(assembly_time) > range_ts else assembly_time
+                linear_solver_time = linear_solver_time[-range_ts:] if len(linear_solver_time) > range_ts else linear_solver_time
+                step_start_time = step_start_time[-range_ts:] if len(step_start_time) > range_ts else step_start_time
+                iteration_number = iteration_number[-range_ts:] if len(iteration_number) > range_ts else iteration_number
+                data_source.data = {"step_size": step_size, "time_step": time_step, "assembly_time": assembly_time, "linear_solver_time": linear_solver_time, "step_start_time": step_start_time, "iteration_number": iteration_number}
+
         elif isinstance(item, AssemblyTime):
             index = len(data_source.data["assembly_time"])-1
             new_time = data_source.data["assembly_time"][index] + item.assembly_time
@@ -152,12 +199,35 @@ def update_figure():
             iteration = item.iteration_number
             data_source.patch({"iteration_number": [(index, iteration)]})
         elif isinstance(item, IterationStart):
-            iteration_offset = len(data_source_iter.data["iteration_number"])
+            iteration_offset = data_source_iter.data["iteration_number"][-1] if data_source_iter.data["iteration_number"] else 0
             line_width = 0
             if item.iteration_number == 1:
                 line_width = 1
-            new_row = {"iteration_number": [iteration_offset+1], "vspan": [iteration_offset+0.75], "line_width": [line_width], "dx_x": [1], "dx_x_0": [1], "dx_x_1": [1],"dx_x_2": [1], "dx_x_3": [1], "dx_x_4": [1],"dx_x_5": [1]}
-            data_source_iter.stream(new_row)
+            if range_iter == 0:
+                new_row = {"iteration_number": [iteration_offset+1], "vspan": [iteration_offset+0.75], "line_width": [line_width], "dx_x": [1], "dx_x_0": [1], "dx_x_1": [1],"dx_x_2": [1], "dx_x_3": [1], "dx_x_4": [1],"dx_x_5": [1]}
+                data_source_iter.stream(new_row)
+            else:
+                iteration_number = data_source_iter.data["iteration_number"] + [iteration_offset+1]
+                vspan = data_source_iter.data["vspan"] + [iteration_offset+0.75]
+                line_width = data_source_iter.data["line_width"] + [line_width]
+                dx_x = data_source_iter.data["dx_x"] + [1]
+                dx_x_0 = data_source_iter.data["dx_x_0"] + [1]
+                dx_x_1 = data_source_iter.data["dx_x_1"] + [1]
+                dx_x_2 = data_source_iter.data["dx_x_2"] + [1]
+                dx_x_3 = data_source_iter.data["dx_x_3"] + [1]
+                dx_x_4 = data_source_iter.data["dx_x_4"] + [1]
+                dx_x_5 = data_source_iter.data["dx_x_5"] + [1]
+                iteration_number = iteration_number[-range_iter:] if len(iteration_number) > range_iter else iteration_number
+                vspan = vspan[-range_iter:] if len(vspan) > range_iter else vspan
+                line_width = line_width[-range_iter:] if len(line_width) > range_iter else line_width
+                dx_x = dx_x[-range_iter:] if len(dx_x) > range_iter else dx_x
+                dx_x_0 = dx_x_0[-range_iter:] if len(dx_x_0) > range_iter else dx_x_0
+                dx_x_1 = dx_x_1[-range_iter:] if len(dx_x_1) > range_iter else dx_x_1
+                dx_x_2 = dx_x_2[-range_iter:] if len(dx_x_2) > range_iter else dx_x_2
+                dx_x_3 = dx_x_3[-range_iter:] if len(dx_x_3) > range_iter else dx_x_3
+                dx_x_4 = dx_x_4[-range_iter:] if len(dx_x_4) > range_iter else dx_x_4
+                dx_x_5 = dx_x_5[-range_iter:] if len(dx_x_5) > range_iter else dx_x_5
+                data_source_iter.data = {"iteration_number": iteration_number, "vspan": vspan, "line_width": line_width, "dx_x": dx_x, "dx_x_0": dx_x_0, "dx_x_1": dx_x_1,"dx_x_2": dx_x_2, "dx_x_3": dx_x_3, "dx_x_4": dx_x_4,"dx_x_5": dx_x_5}
         elif isinstance(item, TimeStepConvergenceCriterion):
             index = len(data_source_iter.data["iteration_number"])-1
             data_source_iter.patch({f"dx_x": [(index, item.dx_x)]})
@@ -182,6 +252,10 @@ def newplot_lin(attr, old, new):
                 ("Iteration Number", "@iteration_number")
                 ])
         fig_1.add_tools(hover)
+        #range_end = data_source.data["time_step"][-1]
+        #range_start = range_end - 10 if range_end > 10 else 0
+        #print(f"Setting x_range to {range_start} - {range_end}")
+        #fig_1.x_range = Range1d(start=range_start, end=range_end)
     elif new == "Assembly Time":
         fig_1.line(x="time_step", y="assembly_time", line_color="blue", line_width=3.0, source=data_source)
         fig_1.xaxis.axis_label = "time_step (s)"
@@ -317,11 +391,40 @@ def newplot_log(attr, old, new):
                 ])
         fig_2.add_tools(hover)
 
-curdoc().add_periodic_callback(update_figure, 1)
+curdoc().add_periodic_callback(update_figure, update_interval)
 
+
+#def start_stop_observer():
+#    global observer, block, handler, original_file, button
+#    if observer.is_alive():
+#        print("Stopping observer...")
+#        block = False
+#        observer.stop()
+        #observer.join()
+#        button.label = "Observer Stopped."
+#        button.button_type = "success"
+#    else:
+#        print("Starting observer...")
+#        block = True
+#        os.utime(original_file, None)  # Update the file's last modified time to ensure it is read from the beginning
+        #observer.schedule(handler, path=str(original_file.parent), recursive=False)
+#        observer.start()
+#        button.label = "Observer running."
+#        button.button_type = "danger"
+#button.on_click(start_stop_observer)
+def update_slider_ts(attr, old, new):
+    global range_ts, slider_ts
+    range_ts = new
+    slider_ts.value = new
+def update_slider_iterations(attr, old, new):
+    global range_iter, slider_iterations
+    range_iter = new
+    slider_iterations.value = new
+slider_ts.on_change("value", update_slider_ts)
+slider_iterations.on_change("value", update_slider_iterations)
 dropdown_lin.on_change("value", newplot_lin)
 dropdown_log.on_change("value", newplot_log)
-layout = column(page_logo, row(column(dropdown_lin, fig_1), column(dropdown_log, fig_2)))
+layout = column(page_logo, row(slider_ts, slider_iterations), row(column(dropdown_lin, fig_1), column(dropdown_log, fig_2)))
 
 curdoc().add_root(layout)
 curdoc().title = "OGS Log Monitor"
